096e4d24e3d9ab21260b2e1addc753367de5a42d
zremud <dumdumpaulorlan06@gmail.com>
2018-02-01 22:58:26 +0800

    Added the initial comment function and bootstrap for the front-end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5 }
+  has_many :comments
+  validates_associated :comments
+  validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :text, presence: true
 end
