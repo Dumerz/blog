1eeb0b4fabd93b2af596b55f6dad061dca394234
zremud <dumdumpaulorlan06@gmail.com>
2018-02-10 13:39:45 +0800

    Updated the timestamp format of articles and comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	# test if link is the current path
+	# return 'active'
+	def current_path?(test_path)
+		return 'class = active' if request.path == test_path
+		''
+	end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, -> { order('created_at DESC') }, class_name: 'Comment'
   validates_associated :comments
   validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
   validates :text, presence: true
